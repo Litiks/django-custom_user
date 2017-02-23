4190aa29bfa4c973e0be60098031d8f5be484ead
Aaron Mader <a.mader@gmail.com>
2017-02-23 18:09:50 -0500

    tweaking the migration

--- custom_user/migrations/0002_emailuser_person_version.py
+++ custom_user/migrations/0002_emailuser_person_version.py
@@ -7,7 +7,6 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('person', '0002_auto_20150630_1640'),
         ('custom_user', '0001_initial'),
     ]
 
