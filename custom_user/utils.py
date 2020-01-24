2c40c4fd45b47c61f65ccc153eef33699cf783a3
Aaron Mader <a.mader@gmail.com>
2020-01-24 11:44:54 -0500

    Adding a setting, to opt-out of using zxcvbn

--- custom_user/utils.py
+++ custom_user/utils.py
@@ -1,8 +1,12 @@
 from __future__ import unicode_literals
 from django import forms
+from django.conf import settings
+from django.contrib.auth.password_validation import validate_password
 from zxcvbn import zxcvbn
 
 def clean_password(password):
+    zxcvbn_enabled = getattr(settings, 'CUSTOM_USER_ENABLE_ZXCVBN', True)
+    if zxcvbn_enabled:
         user_inputs = [
             'doubleh',
             #possibly include user fields like the user's email address and name
@@ -17,5 +21,7 @@ def clean_password(password):
         # as an added measure, consider checking the password against existing passwords in the database.
         # https://diogomonica.com/posts/password-security-why-the-horse-battery-staple-is-not-correct/
 
-        return password
+    else:
+        validate_password(password, user=None)
 
+    return password
