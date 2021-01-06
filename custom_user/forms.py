e777563207b9f0743bc902bd9026406fa81f046c
Aaron Mader <aaron@litiks.com>
2021-01-06 11:05:33 -0500

    Modifying the link url.

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -135,7 +135,7 @@ class EmailUserChangeForm(forms.ModelForm):
     password = ReadOnlyPasswordHashField(label=_("Password"), help_text=_(
         "Raw passwords are not stored, so there is no way to see "
         "this user's password, but you can change the password "
-        "using <a href=\"password/\">this form</a>."))
+        "using <a href=\"../password/\">this form</a>."))
 
     class Meta:
         model = get_user_model()
