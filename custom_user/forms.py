12296858b03fd8976baeb38a8e8a5e304075710a
Aaron Mader <aaron@litiks.com>
2022-12-07 10:05:57 -0500

    Some updates for django 4.1

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth.hashers import make_password
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from allauth.account.forms import LoginForm
 from captcha.fields import CaptchaField
